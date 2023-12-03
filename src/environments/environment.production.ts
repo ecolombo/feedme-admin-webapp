8b7c6597ac90dffd28d475408710aeb8e532869e
ecolombo <elia.colombo@gmail.com>
2023-12-03 15:40:57 +0100

    vor Erzeuge guard

--- src/environments/environment.production.ts
+++
@@ -1,4 +0,0 @@
-export const environment = {
-    production: true,
-    apiBaseUrl :'http://localhost:4401/feedme-webservice-admin/api'
-};
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,1 +1,4 @@
-export const environment = {};
+export const environment = {
+    production: true,
+    apiBaseUrl :'http://localhost:4401/feedme-webservice-admin/api'
+};
